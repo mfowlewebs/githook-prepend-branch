094b7ea77953e02faf8ec4c19c54b0105b7dfbee
Matthew Fowle <matthewf@webs.com>
2018-10-31 17:12:44 -0400

    Check to make sure we found a ticket. Simplify output.

--- prepare-commit-msg.sh
+++ prepare-commit-msg.sh
@@ -8,11 +8,15 @@ fi
 
 ORIGINAL_BRANCH_NAME=$(git symbolic-ref --short HEAD)
 STRIPPED_BRANCH_NAME=${ORIGINAL_BRANCH_NAME##*/}
-BRANCH_NAME=$(sed -e 's/^.*\/\(DG-[[:digit:]]\+\).*$/\1/g' <<<STRIPPED_BRANCH_NAME)
+#BRANCH_NAME=$(echo $STRIPPED_BRANCH_NAME | sed -e 's/^.*\/\(DG-[[:digit:]]\+\).*$/x/g')
+BRANCH_NAME=$(sed -e 's/\(DG-[[:digit:]]\+\).*/\1/g' <<< $STRIPPED_BRANCH_NAME)
+
 
 BRANCH_EXCLUDED=$(printf "%s\n" "${BRANCHES_TO_SKIP[@]}" | grep -c "^$BRANCH_NAME$")
 BRANCH_IN_COMMIT=$(grep -c "\[$BRANCH_NAME\]" $1)
 
-if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]] && [[ "$BRANCH_NAME" -ne "$STRIPPED_BRANCH_NAME" ]] ; then
-  sed -i.bak -e "1s/^/[$BRANCH_NAME] /" $1
+echo >> LOG
+if [ -n "$BRANCH_NAME" ] && ! [[ $BRANCH_EXCLUDED -eq 1 ]] && ! [[ $BRANCH_IN_COMMIT -ge 1 ]] && [ "$BRANCH_NAME" != "$STRIPPED_BRANCH_NAME" ] ; then
+  echo OK >> LOG
+  sed -i.bak -e "1s/^/$BRANCH_NAME /" $1
 fi
